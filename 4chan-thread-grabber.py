c6273c15d98d8490633d1fad798a39549bdef627
William Setzer <wjsetzer@uab.edu>
2018-10-30 14:46:22 -0500

    added repl

--- 4chan-thread-grabber.py
+++ 4chan-thread-grabber.py
@@ -1,7 +1,8 @@
 #! /usr/bin/env python
 
+import json
 import os
-import shutil
+import re
 import sys
 import time
 
@@ -53,29 +54,88 @@ def get_images(save_location, board, thread_num):
     with open(f'{path}/{image}', 'wb') as f:
       # r.raw.decode_content = True
       f.write(image_bin)
-      # shutil.copyfileobj(image_bin, f)
 
     print(f'DONE {idx + 1} OUT OF {len(images)}')
 
-def main(args):
+def repl(save_location, board):
+
+  def helpmsg():
+  
+    print('Type the name of the thread to grab. Type "help" to show this message. Type "quit" or "q" to quit')
+
+  helpmsg()
+
+  while True:
+
+    user_input = input(' > ')
+
+    if re.match(f'\d+', user_input):
+      try:
+        get_images(save_location, board, user_input)
+      except json.decoder.JSONDecodeError:
+        print("Check to ensure thread number is correct and that you are not blocked")
+        print("To check if you're blocked, visit http://a.4cdn.org/boards.json and confirm the site does not perform a browser check.")
+    elif user_input == 'q' or user_input == 'quit':
+      confirm = input('Are you sure you want to quit? Y/n: ')
+      if 'y' in confirm.lower():
+        quit()
+    else:
+      helpmsg()
+    
+
+def parse_args(args):
+  # positional args:
+  # save location
+  # board
+  
+  # optional position args:
+  # thread_num. If repl is set, ignored, else required
+
+  # flags
+  # -r / --repl : pseudo repl. allow user to repeatedly grab threads
 
   if len(args) < 3:
     msg = """
     ARGUMENTS:
-      4chan-thread-image-grabber.py <save location> <board> <thread>
+      4chan-thread-image-grabber.py <save location> <board> [<thread>|-r|--repl]
 
       save location: e.g. Y:4chan_stuff/midterm
       board: e.g. pol
       thread: e.g. 111111111
+
+      -r | --repl : start in REPL mode to keep entering threads
     """
     print(msg)
     quit()
 
-  save_location = args[0]
-  board = args[1]
-  thread_num= args[2]
+  parsed_args = dict()
 
-  get_images(save_location, board, thread_num)
+  if '-r' in args or '--repl' in args:
+    parsed_args['repl'] = True
+
+    # in case someone did it with classic unix flags before positional args
+    if '-r' in args:
+      args.remove('-r')
+    else:
+      args.remove('--repl')
+  else:
+    # thread is required if repl not set
+    parsed_args['thread'] = args[2]
+
+  parsed_args['save'] = args[0]
+  parsed_args['board'] = args[1]
+  
+  return parsed_args
+
+
+def main(args):
+
+  p_args = parse_args(args)
+
+  if not p_args['repl']:
+    get_images(p_args['save'], p_args['board'], p_args['thread'])
+  else:
+    repl(p_args['save'], p_args['board'])
 
 if __name__ == '__main__':
   main(sys.argv[1:])
